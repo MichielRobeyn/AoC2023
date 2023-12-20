08cd38cada883e6ff8a7efb9041c7ab1c4b8ec88
Michiel Robeyn <michiel.robeyn@outlook.com>
2023-12-20 23:10:48 +0100

    Initial commit

--- day16-20/day20b_propagation.py
+++ day16-20/day20b_propagation.py
@@ -1,3 +1,30 @@
+from functools import reduce
+from copy import deepcopy
+
+
+def chinese_remainder(m, a):
+    total_sum = 0
+    prod = reduce(lambda acc, b: acc * b, m)
+    for n_i, a_i in zip(m, a):
+        p = prod // n_i
+        total_sum += a_i * mul_inv(p, n_i) * p
+    return total_sum % prod
+
+
+def mul_inv(a, b):
+    b0 = b
+    x0, x1 = 0, 1
+    if b == 1:
+        return 1
+    while a > 1:
+        q = a // b
+        a, b = b, a % b
+        x0, x1 = x1 - q * x0, x0
+    if x1 < 0:
+        x1 += b0
+    return x1
+
+
 class Broadcaster:
 
     def __init__(self):
@@ -94,6 +121,29 @@ class Output:
         return self.name
 
 
+def get_cycle_offset(objects, end):
+    i = 0
+    seen = False
+    running = True
+    while running:
+        messages = [objects['broadcaster'].send()]
+        while messages:
+            next_nodes, message, sender = messages[0]
+            del (messages[0])
+            for next_node in next_nodes:
+                if next_node == objects['qb'] and message and sender == end and seen:
+                    running = False
+                elif next_node == objects['qb'] and message and sender == end:
+                    seen = True
+                    offset = i
+                response = next_node.catch(message, sender)
+                if response is not None:
+                    messages.append(response)
+        i += 1
+    cycle = i - offset - 1
+    return cycle, offset
+
+
 def main():
     data = dict()
     with open('day20_input.txt') as file:
@@ -125,25 +175,14 @@ def main():
                 if el[1:] in data[el2]:
                     objects[el[1:]].add_inputs(el2[1:])
 
-    total_low = 0
-    total_high = 0
-    i = 0
-    while i < 1000:
-        messages = [objects['broadcaster'].send()]
-        total_low += 1
-        while messages:
-            next_nodes, message, sender = messages[0]
-            if message:
-                total_high += len(next_nodes)
-            else:
-                total_low += len(next_nodes)
-            del(messages[0])
-            for next_node in next_nodes:
-                response = next_node.catch(message, sender)
-                if response is not None:
-                    messages.append(response)
-        i += 1
-    print(total_low * total_high)
+    kv = get_cycle_offset(deepcopy(objects), 'kv')
+    jg = get_cycle_offset(deepcopy(objects), 'jg')
+    rz = get_cycle_offset(deepcopy(objects), 'rz')
+    mr = get_cycle_offset(deepcopy(objects), 'mr')
+
+    multipliers = [kv[0], jg[0], rz[0], mr[0]]
+    offsets = [kv[1], jg[1], rz[1], mr[1]]
+    print(chinese_remainder(multipliers, offsets) + 1)
 
 
 if __name__ == '__main__':
